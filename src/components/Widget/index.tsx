b4fda11652189a19ab0b15c4cfa2f56cfffcac8d
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2022-05-11 00:46:28 -0300

    Uploading project to the github

---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,66 @@
+import { useRef, useState } from "react";
+import { TouchableOpacity } from "react-native";
+import { ChatTeardropDots } from "phosphor-react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+
+import { Options } from "../Options";
+import { Form } from "../Form";
+import { Success } from "../Success";
+
+import { styles } from "./styles";
+import { theme } from "../../theme";
+import { feedbackTypes } from "../../utils/feedbackTypes";
+
+export type IFeedbackType = keyof typeof feedbackTypes;
+
+function Widget() {
+  const [feedbackType, setFeedbackType] = useState<IFeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  function handleFeedbackSent() {
+    setFeedbackSent(true);
+  }
+  return (
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
+        <ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        {feedbackSent ? (
+          <Success onSendAnotherFeedback={handleRestartFeedback} />
+        ) : (
+          <>
+            {feedbackType ? (
+              <Form
+                feedbackType={feedbackType}
+                onFeedbackCanceled={handleRestartFeedback}
+                onFeedbackSent={handleFeedbackSent}
+              />
+            ) : (
+              <Options onFeedbackTypeChanged={setFeedbackType} />
+            )}
+          </>
+        )}
+      </BottomSheet>
+    </>
+  );
+}
+
+export default gestureHandlerRootHOC(Widget);
